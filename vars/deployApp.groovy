bec71a461271d8a0aa8de0ef4bea1da4e7a1d298
Amgad Ashraf <amgad.ashraf96@gmail.com>
2024-07-17 00:30:40 +0000

    edit servicces type

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,16 +1,17 @@
 def call() {
-	sshagent(['Master_Node_SSH_Cred']) {
-                        sh """
-                        ssh -o StrictHostKeyChecking=no ubuntu@54.226.138.43 <<'EOF'
-                        git clone -b App_Kube_cluster https://github.com/amgad96/Full_React_App.git
-                        cd Full_React_App
-                        sudo kubectl apply -f DB-PersistentVol.yaml
-                        sudo kubectl apply -f MongoDB-DS.yaml
-                        sudo kubectl apply -f Backend-DS.yaml
-                        sudo kubectl apply -f Frontend-DS.yaml
-                        cd ..
-                        rm -rf Full_React_App
+    sshagent(['Master_Node_SSH_Cred']) {
+        sh """
+        ssh -o StrictHostKeyChecking=no ubuntu@54.226.138.43 << 'EOF'
+        git clone -b App_Kube_cluster https://github.com/amgad96/Full_React_App.git
+        cd Full_React_App
+        sudo kubectl apply -f DB-PersistentVol.yaml
+        sudo kubectl apply -f MongoDB-DS.yaml
+        sudo kubectl apply -f Backend-DS.yaml
+        sudo kubectl apply -f Frontend-DS.yaml
+        cd ..
+        rm -rf Full_React_App
 EOF
-                        """
-                    }       
+        """
+    }
 }
+
