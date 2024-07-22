7dea608ab87ed0b96f47b0892614b17f8bde1025
Amgad Ashraf <amgad.ashraf96@gmail.com>
2024-07-22 11:34:14 +0000

    edit yaml file

--- run_yamls.sh
+++ run_yamls.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf clusterDir
 git clone -b App_Kube_cluster https://github.com/amgad96/Full_React_App.git clusterDir
-cd Full_React_App
+cd clusterDir
 sudo kubectl apply -f DB-PersistentVol.yaml
 sudo kubectl apply -f MongoDB-DS.yaml
 sudo kubectl apply -f Backend-DS.yaml
