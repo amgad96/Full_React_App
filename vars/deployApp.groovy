fd6f2ceea2e98409719e837369f5d6f031cd4648
Amgad Ashraf <amgad.ashraf96@gmail.com>
2024-07-22 11:27:32 +0000

    try yamls script

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,5 +1,10 @@
 def call() {
+        // Download the script file 
+        sh 'wget -O ./run_yamls.sh https://raw.githubusercontent.com/amgad96/Full_React_App/main/run_yamls.sh'
 	sshagent(['Master_Node_SSH_Cred']) {
-        sh 'ssh -o StrictHostKeyChecking=no ubuntu@remote-server "./run_yamls.sh"'
+                // Copy the script to the remote server
+                sh 'scp -o StrictHostKeyChecking=no ./run_yamls.sh ubuntu@54.196.238.157:/home/ubuntu/run_yamls.sh'
+                // Execute the script on the remote server
+                sh 'ssh -o StrictHostKeyChecking=no ubuntu@54.196.238.157 "chmod +x /home/ubuntu/run_yamls.sh && /home/ubuntu/run_yamls.sh"'
         }          
 }
