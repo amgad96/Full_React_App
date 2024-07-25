f55b29efa5561cbad7f844177ac64cc562e232b2
Amgad Ashraf <amgad.ashraf96@gmail.com>
2024-07-25 12:29:53 +0000

    add the backend load balancer to the frontent code

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       setIsLoading(true);
 
       try {
-        const response = await fetch('http://139.59.149.177:30080/goals');
+        const response = await fetch('Backend-LB-558042472.us-east-1.elb.amazonaws.com/goals');
 
         const resData = await response.json();
 
@@ -39,7 +39,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://139.59.149.177:30080/goals', {
+      const response = await fetch('Backend-LB-558042472.us-east-1.elb.amazonaws.com/goals', {
         method: 'POST',
         body: JSON.stringify({
           text: goalText,
@@ -78,7 +78,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://139.59.149.177:30080/goals/' + goalId, {
+      const response = await fetch('Backend-LB-558042472.us-east-1.elb.amazonaws.com/goals/' + goalId, {
         method: 'DELETE',
       });
 
